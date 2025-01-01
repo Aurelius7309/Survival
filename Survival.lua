cb3b78cb347111b148646c81f82d72b0a13a45c7
Casimir Eisenach <casimirusenormus@hotmail.de>
2025-01-01 22:40:06 +0100

    Resurrect this mod

--- Survival.lua
+++ Survival.lua
@@ -1,112 +1,96 @@
---- STEAMODDED HEADER
---- MOD_NAME: Survival Mode
---- MOD_ID: Survival
---- MOD_AUTHOR: [Aure]
---- MOD_DESCRIPTION: This mod adds two new game modes: Survival and Survival+. In both modes, the game is extended to last for 16 Antes, with final Boss Blinds every 4 Antes. Ante scaling from Ante 9 to Ante 16 have been adjusted by varying amounts depending on stake. On Survival+ mode, you start with 2 less joker slots, but gain an additional joker slot after defeating a final Boss Blind.
-
-----------------------------------------------
-------------MOD CODE -------------------------
-
-function SMODS.INIT.Survival()
-    local Game_init_game_object_ref = Game.init_game_object
-    function Game:init_game_object()
-        local GAME = Game_init_game_object_ref(self)
-        if G.survival or G.survival_plus then
-            GAME.win_ante = 16
-            GAME.showdown_ante = 4
-        end
-        if G.survival_plus then
-            GAME.survival_plus = true
-            GAME.starting_params.joker_slots = 3
-        end
-        return GAME
-    end
-
-    local get_new_boss_ref = get_new_boss
-    function get_new_boss()
-        local win_ante = G.GAME.win_ante
-        G.GAME.win_ante = G.GAME.showdown_ante or win_ante
-        local boss = get_new_boss_ref()
-        G.GAME.win_ante = win_ante
-        return boss
-    end
-
-    function get_blind_amount(ante)
-        local k = 0.75
-        local amounts = {}
-        if not G.GAME.modifiers.scaling or G.GAME.modifiers.scaling == 1 then
-            amounts = {
-                300, 800, 2800, 6000, 11000, 20000, 35000, 50000,
-                90000, 150000, 260000, 450000, 800000, 1400000, 2400000, 4300000
-            }
-        elseif G.GAME.modifiers.scaling == 2 then
-            amounts = {
-                300, 1000, 3200, 9000, 18000, 32000, 56000, 90000,
-                180000, 400000, 1000000, 2400000, 6000000, 16000000, 45000000, 135000000
-            }
-        elseif G.GAME.modifiers.scaling == 3 then
-            amounts = {
-                300, 1200, 3600, 10000, 25000, 50000, 90000, 180000,
-                480000, 1600000, 6000000, 22000000, 90000000, 400000000, 2000000000, 12000000000
-            }
-        end
-        if ante < 1 then return 100 end
-        if ante <= 16 then return amounts[ante] end
-        local a, b, c, d = amounts[16], 1.6, ante - 16, 1 + 0.2 * (ante - 16)
-        local amount = math.floor(a * (b + (k * c) ^ d) ^ c)
-        amount = amount - amount % (10 ^ math.floor(math.log10(amount) - 1))
-        return amount
-    end
+local Survival = SMODS.current_mod
+Survival.config_tab = function()
+    return {n = G.UIT.ROOT, config = {r = 0.1, minw = 4, align = "tm", padding = 0.2, colour = G.C.BLACK}, nodes = {
+        create_option_cycle({
+            label = localize('k_survival_mode'),
+            scale = 0.8,
+            w = 4,
+            options = localize('ml_survival_opt'),
+            info = {localize('k_survival_info')},
+            opt_callback = 'survival_change_mode',
+            current_option = Survival.config.mode or 2,
+        })
+}}
+end
+G.FUNCS.survival_change_mode = function(e)
+    Survival.config.mode = e.to_key
 end
 
+local Game_init_game_object_ref = Game.init_game_object
+function Game:init_game_object()
+    local ret = Game_init_game_object_ref(self)
+    ret.survival = Survival.config.mode
+    if ret.survival >= 2 then
+        ret.win_ante = 16
+        ret.showdown_ante = 4
+    end
+    if ret.survival >= 3 then
+        ret.starting_params.joker_slots = ret.starting_params.joker_slots - 2
+    end
+    return ret
+end
 
-	function G.FUNCS.change_survival(args)
-		G.survival = args.to_key == 2
-		G.survival_plus = args.to_key == 3
-	end
+local get_new_boss_ref = get_new_boss
+function get_new_boss()
+    local win_ante = G.GAME.win_ante
+    G.GAME.win_ante = G.GAME.showdown_ante or win_ante
+    local boss = get_new_boss_ref()
+    G.GAME.win_ante = win_ante
+    return boss
+end
 
-local run_setup_option_ref = G.UIDEF.run_setup_option
-function G.UIDEF.run_setup_option(type)
-	local t = run_setup_option_ref(type)
-	if type == 'New Run' then
-		--t.nodes[5] = t.nodes[4]
-		t.nodes[5] = create_option_cycle({
-			options = {
-				'Off',
-				'Survival',
-				'Survival+'
-            },
-			label = 'Survival Mode',
-			cycle_shoulders = true,
-			opt_callback = 'change_survival',
-            current_option = G.survival_plus and 3 or G.survival and 2 or 1,
-            scale = 0.8,
-			w = 4
-		})
-	end
-	return t
+local to_big = to_big or function(x) return x end
+local gba = get_blind_amount
+function get_blind_amount(ante)
+    if G.GAME.survival < 2 or (G.GAME.modifiers.scaling or 1) > 3 then return gba(ante) end
+    local k = 0.75
+    local amounts = {}
+    if not G.GAME.modifiers.scaling or G.GAME.modifiers.scaling == 1 then
+        amounts = {
+            to_big(300), to_big(800), to_big(2800), to_big(6000), to_big(11000), to_big(20000), to_big(35000), to_big(50000),
+            to_big(9e4), to_big(1.5e5), to_big(2.6e5), to_big(4.5e5), to_big(8e5), to_big(1.4e6), to_big(2.4e6), to_big(4.3e6)
+        }
+    elseif G.GAME.modifiers.scaling == 2 then
+        amounts = {
+            to_big(300), to_big(1000), to_big(3200), to_big(9000), to_big(18000), to_big(32000), to_big(56000), to_big(90000),
+            to_big(1.8e5), to_big(4e5), to_big(1e6), to_big(2.4e6), to_big(6e6), to_big(1.6e7), to_big(4.5e7), to_big(1.4e8)
+        }
+    elseif G.GAME.modifiers.scaling == 3 then
+        amounts = {
+            to_big(300), to_big(1200), to_big(3600), to_big(10000), to_big(25000), to_big(50000), to_big(90000), to_big(1.8e5),
+            to_big(4.8e5), to_big(1.6e6), to_big(6e6), to_big(2.2e7), to_big(9e7), to_big(4e8), to_big(2e9), to_big(1.2e10)
+        }
+    end
+    if ante < 1 then return 100 end
+    if ante <= 16 then return amounts[ante] end
+    local a, b, c, d = amounts[16], 1.6, ante - 16, 1 + 0.2 * (ante - 16)
+    local amount = math.floor(a * (b + (k * c) ^ d) ^ c)
+    amount = amount - amount % (10 ^ math.floor(math.log10(amount) - 1))
+    return amount
 end
 
 local set_blind_ref = Blind.set_blind
 function Blind:set_blind(blind, reset, silent)
-	set_blind_ref(self, blind, reset, silent)
+    set_blind_ref(self, blind, reset, silent)
     G.GAME.last_blind.showdown = blind and blind.boss and blind.boss.showdown or nil
 end
 
 local evaluate_round_ref = G.FUNCS.evaluate_round
 function G.FUNCS.evaluate_round()
-	evaluate_round_ref()
-	if G.GAME.survival_plus and G.GAME.last_blind and G.GAME.last_blind.boss and G.GAME.last_blind.showdown then
-		G.E_MANAGER:add_event(Event({
-			func = function()
+    evaluate_round_ref()
+    if G.GAME.survival >= 3 and G.GAME.last_blind and G.GAME.last_blind.boss and G.GAME.last_blind.showdown then
+        G.E_MANAGER:add_event(Event({
+            func = function()
                 if G.jokers then
                     G.jokers.config.card_limit = G.jokers.config.card_limit + 1
+                    card_eval_status_text(G.deck.cards[1], 'extra', nil, nil, nil, {
+                        message = localize { type = 'variable', key = 'ml_negative_desc', vars = {1}}[2],
+                        colour = G.C.MONEY,
+                        instant = true
+                    })
                 end
-				return true
-			end
-		}))
-	end
+                return true
+            end
+        }))
+    end
 end
-
-----------------------------------------------
-------------MOD CODE END----------------------
---
+++ localization/en-us.lua
@@ -0,0 +1,13 @@
+return {
+    misc = {
+        dictionary = {
+            k_survival_mode = 'Survival Mode',
+            k_survival_info = 'Changes apply to new runs',
+            ml_survival_opt = {
+                'Disabled',
+                'Survival',
+                'Survival+',
+            }
+        }
+    }
+}
